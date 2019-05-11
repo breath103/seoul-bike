a44193ecae7be096871b0145a40820533d851fea
Kurt Lee <breath103@gmail.com>
2019-05-11 14:02:38 +0900

    add typings

--- backend/src/cron/index.ts
+++ backend/src/cron/index.ts
@@ -16,26 +16,32 @@ export async function handler() {
     }
   );
 
-  const stations = (response.data.realtimeList as object[])
-    .filter((s: any) => {
-      return s.stationUseYn === "Y" &&
-        Number(s.stationLatitude) !== 0 &&
-        Number(s.stationLongitude) !== 0;
-    })
-    .map((s: any) => {
-      const name = (() => {
-        const res = s.stationName.match(/(\d+).(.+)/i);
-        return ((res && res[2]) || s.stationName) as string;
-      })();
-      return {
-        name,
-        coordinate: {
-          latitude: Number(s.stationLatitude),
-          longitude: Number(s.stationLongitude),
-        },
-        availableBikes: Number(s.parkingBikeTotCnt),
-      };
-    });
+  const stations = (response.data.realtimeList as Array<{
+    stationUseYn: "Y" | "N",
+    stationLatitude: string,
+    stationLongitude: string,
+    stationName: string,
+    parkingBikeTotCnt: string,
+  }>)
+  .filter((s) => {
+    return s.stationUseYn === "Y" &&
+      Number(s.stationLatitude) !== 0 &&
+      Number(s.stationLongitude) !== 0;
+  })
+  .map((s) => {
+    const name = (() => {
+      const res = s.stationName.match(/(\d+).(.+)/i);
+      return (((res && res[2]) || s.stationName) as string).trim();
+    })();
+    return {
+      name,
+      coordinate: {
+        latitude: Number(s.stationLatitude),
+        longitude: Number(s.stationLongitude),
+      },
+      availableBikes: Number(s.parkingBikeTotCnt),
+    };
+  });
 
   logger("Stations: %d, %d", response.data.realtimeList.length, stations.length);
 
