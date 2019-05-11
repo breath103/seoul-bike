dadfd0b9ea70931eb3e635e38b806180499fa75f
Kurt Lee <breath103@gmail.com>
2019-05-11 12:00:09 +0900

    fix ts / apps

---
+++ backend/src/cron/__test__/index_spec.ts
@@ -0,0 +1,7 @@
+import { handler } from "../index";
+
+describe("Cron.Handler", () => {
+  it("should work", async () => {
+    await handler();
+  });
+});
--- backend/src/cron/index.ts
+++ backend/src/cron/index.ts
@@ -1,20 +1,24 @@
 import * as AWS from "aws-sdk";
 import Axios from "axios";
+import * as debug from "debug";
 
-export async function handler(event: any) {
-  const response = await Axios.get("https://www.bikeseoul.com/app/station/getStationRealtimeStatus.do");  
-  
-  const stations = (response.data.realtimeList as Array<any>)
+const logger = debug("Cron");
+const s3 = new AWS.S3();
+
+export async function handler() {
+  const response = await Axios.post("https://www.bikeseoul.com/app/station/getStationRealtimeStatus.do");
+
+  const stations = (response.data.realtimeList as any[])
     .filter((s: any) => {
-      return s.stationUseYn === 'Y' &&
+      return s.stationUseYn === "Y" &&
         Number(s.stationLatitude) !== 0 &&
         Number(s.stationLongitude) !== 0;
     })
     .map((s: any) => {
       const name = (() => {
-        const res = s.stationName.match(/(\d+).(.+)/i)
+        const res = s.stationName.match(/(\d+).(.+)/i);
         return ((res && res[2]) || s.stationName) as string;
-      })()
+      })();
       return {
         name,
         coordinate: {
@@ -22,16 +26,14 @@ export async function handler(event: any) {
           longitude: Number(s.stationLongitude),
         },
         availableBikes: Number(s.parkingBikeTotCnt),
-      }
+      };
     });
 
-  console.log(JSON.stringify(stations.slice(0, 10)));
-    
-  console.log("Upload");
-  const s3 = new AWS.S3();
+  logger("Stations: %O", stations.slice(0, 10));
+
   await s3.putObject({
     Bucket: process.env.ASSET_BUCKET as string,
-    Key: 'stations.json',
+    Key: "stations.json",
     Body: JSON.stringify({
       stations,
       meta: {
@@ -42,7 +44,5 @@ export async function handler(event: any) {
     ContentType: "application/json",
   }).promise();
 
-  console.log("Done");
-  // Upload this to S3? hm..
-  // but then cloudfront could be even more expensive
+  logger("Done");
 }
